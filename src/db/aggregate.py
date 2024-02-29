ddbce576c91f96721fad6577d3a369c12a8227be
Ivan Stasevich <pravitel2015ify@gmail.com>
2024-02-29 14:28:32 +0300

    Add .gitignore

--- src/db/aggregate.py
+++ src/db/aggregate.py
@@ -1,56 +1,9 @@
 from datetime import datetime
 
-from dateutil.relativedelta import relativedelta
 from pandas import date_range
 from pymongo import MongoClient
 
 from src.schemes import SResponse, SRequest
-
-
-def aggregate_payments_naive(request: SRequest) -> SResponse:
-    dt_from = request.dt_from
-    dt_upto = request.dt_upto
-
-    match request.group_type:
-        case "month":
-            delta = relativedelta(months=1)
-        case "day":
-            delta = relativedelta(days=1)
-        case "hour":
-            delta = relativedelta(hours=1)
-        case _:
-            raise ValueError("Invalid group type")
-
-    client = MongoClient("mongodb://root:example@localhost:27017/")
-    db = client["sampleDB"]
-    collection = db["sample_collection"]
-
-    dataset = []
-    labels = []
-
-    while dt_from < dt_upto:
-        next_dt = dt_from + delta
-
-        result = collection.aggregate(
-            [
-                {"$match": {"dt": {"$gte": dt_from, "$lt": next_dt}}},
-                {"$group": {"_id": None, "total": {"$sum": "$value"}}},
-            ]
-        )
-
-        result_list = list(result)
-
-        total = result_list[0]["total"] if result_list else 0
-        dataset.append(total)
-        labels.append(dt_from)
-
-        dt_from = next_dt
-
-    if dt_from == dt_upto:
-        dataset.append(0)
-        labels.append(dt_from)
-
-    return SResponse(dataset=dataset, labels=labels)
 
 
 def aggregate_payments(request: SRequest) -> SResponse:
